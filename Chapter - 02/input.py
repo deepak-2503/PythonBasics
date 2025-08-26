b8242069b61f07b911f6f0b1f699a3bbdada2390
Deepak Verma <dtv.deepak2503@gmail.com>
2025-08-26 14:13:40 +0530

    take input from user

---
+++ Chapter - 02/input.py
@@ -0,0 +1,15 @@
+""" this progarm will take input as string and when you will sum it then
+it will not sum numbers instead of it will write both string """
+
+# a = input("Enter number 1 : ")
+# b = input("Enter number 2 : ")
+# print(a+b)  
+
+
+# Correct program
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("a is" ,a)
+print("b is" ,b)
+print("sum of a and b is" ,a+b)
