883226240be86df122b99189232170c25c161a9f
Deepak Verma <dtv.deepak2503@gmail.com>
2025-08-26 12:37:50 +0530

    learn all operators

---
+++ Chapter - 02/operators.py
@@ -0,0 +1,40 @@
+# Arithmatic operator
+a = 6
+b = 17
+print(a+b)
+print(a/b)
+print(a-b)
+print(a*b)
+
+# Assignment operator
+a = 9
+b = 2
+a += 4
+print(a)
+b -= 3
+print(b)
+
+# Comparison operators
+k = 4==5
+print(k)
+
+i = 4>6
+print(i)
+
+t = 9<=13
+print(t)
+
+
+# Logical operator
+q = 4>2 and 15<33
+print(q)
+
+_hi = 4>2 and 0>2
+print(_hi)
+
+_or = 2>=2 or 5>51
+print(_or)
+
+
+
+
