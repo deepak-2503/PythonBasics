1b59d63c9fa35da6c09436619e5a540225d0038f
Deepak Verma <dtv.deepak2503@gmail.com>
2025-08-26 13:57:36 +0530

    learn typecasting and one exception also

---
+++ Chapter - 02/TypeCasting.py
@@ -0,0 +1,17 @@
+# We can change type of one funcyion to another if possible 
+a = "31"
+print(type(a))
+b = float(a)
+print(type(b))
+
+
+e = 52.99
+f = int(e)
+print(type(f))
+
+
+k = "deepak"
+l = int(k)
+print(type(l))    # it will throw an error bcoz we cant convert alphabetical string into float or int
+
+
