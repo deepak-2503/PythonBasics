af728a267a1f9bb5623bd13e6584f1bdffd1e5c4
Deepak Verma <dtv.deepak2503@gmail.com>
2025-08-26 12:42:48 +0530

    learn all 5 datatypes

---
+++ Chapter - 02/datatypes.py
@@ -0,0 +1,12 @@
+a = 3 # int datatype
+b = 3.0 # float datatype
+c = "deepak" # string datatype
+d = True or False #Boolean datatype
+e = None  # NOne type
+
+# we can check datatype by type() function
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
