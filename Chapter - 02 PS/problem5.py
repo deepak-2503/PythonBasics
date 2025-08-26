7c1dc1df32e0adb021322718ad1add862234d7f0
Deepak Verma <dtv.deepak2503@gmail.com>
2025-08-26 18:29:13 +0530

    learn one more method for square

--- Chapter - 02 PS/problem5.py
+++ Chapter - 02 PS/problem5.py
@@ -1,4 +1,6 @@
 # Write a python program to calculate the square of a number entered by the user.
 
 number = float(input("Enter a number for square: "))
-print(f"Square of {number} is {number*number}")
+print(f"Square of {number} is {number*number}") #method1
+print(f"Square of {number} is {number**2}") #method2
+print(f"Square of {number} is {number**3}")  # for cube of this number
